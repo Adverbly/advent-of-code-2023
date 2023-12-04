17fbc210c7ad4c5540cfdad2cf7dcb05c5bc3a51
George Montgomery <george.p.montgomery@gmail.com>
2023-12-04 18:41:59 -0500

    d4p1

---
+++ solutions/day-4/part-1/index.ts
@@ -0,0 +1,32 @@
+const solve = (lines: string[]) => {
+  const maxes = [
+    [" red", 12],
+    [" green", 13],
+    [" blue", 14],
+  ] as [string, number][];
+  const result = lines.reduce((sum, line) => {
+    const [gameId, games] = line.split(": ");
+    const [winning, picked] = games
+      .split(" | ")
+      .map((game) => new Set(game.split(" ").filter((str) => str)));
+    const intersection = [...winning].filter((num) => picked.has(num));
+
+    if (intersection.length === 0) {
+      return sum;
+    }
+
+    return sum + 2 ** (intersection.length - 1);
+  }, 0);
+
+  console.log(result);
+};
+// const inputFile = Bun.file(import.meta.dir + "/example.txt");
+const inputFile = Bun.file(import.meta.dir + "/../input.txt");
+const input = await inputFile.text();
+const lines = input.split("\n");
+lines.pop(); // remove trailing newline :(
+
+const start = Bun.nanoseconds();
+solve(lines); // yay for top-level await!
+const end = Bun.nanoseconds();
+console.log(`duration: ${end - start}ns`);
