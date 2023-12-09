06db34155efe0238f24032c16f15321d63fc69a4
George Montgomery <george.p.montgomery@gmail.com>
2023-12-09 15:17:25 -0500

    d9p2

--- solutions/day-9/part-2/Solver.ts
+++ solutions/day-9/part-2/Solver.ts
@@ -1,59 +1,32 @@
-type NodeId = string;
-type Dir = "R" | "L";
-type Children = { R: NodeId; L: NodeId };
-
-const gcd = (a: number, b: number): number => (a ? gcd(b % a, a) : b);
-const lcm = (a: number, b: number) => (a * b) / gcd(a, b);
-const lcmList = (nums: number[]) => nums.reduce(lcm);
-
 export class Solver {
-  dirs: Dir[] = [];
-  nodes: Record<NodeId, Children> = {};
-  dirIndex: number = 0;
-  nodeIds: NodeId[] = [];
-  visited = 0;
+  seqs: number[][] = [];
 
   constructor(lines: string[]) {
     this.parseLines(lines);
   }
 
-  detectCycleLength(nodeId: NodeId): number {
-    let pointer = nodeId;
-    let visited = 0;
-    let dirIndex = 0;
-    while (true) {
-      pointer = this.nodes[pointer][this.dirs[dirIndex % this.dirs.length]];
-
-      dirIndex++;
-      visited++;
-
-      if (pointer[2] === "Z") {
-        return visited;
-      }
+  prevNum(seq: number[]): number {
+    const diffs = [] as number[];
+    for (let index = 0; index < seq.length - 1; index++) {
+      const num = seq[index];
+      const nextNum = seq[index + 1];
+      const diff = nextNum - num;
+      diffs.push(diff);
     }
+    const lastDiff = diffs[diffs.length - 1];
+    const firstSeq = seq[0];
+    if (lastDiff === 0) {
+      return firstSeq;
+    }
+    return firstSeq - this.prevNum(diffs);
   }
 
   solve() {
-    const cycleLengths = this.nodeIds.map((nodeId) =>
-      this.detectCycleLength(nodeId)
-    );
-    return lcmList(cycleLengths);
+    const nextNums = this.seqs.map(this.prevNum.bind(this));
+    return nextNums.reduce((sum, num) => sum + num, 0);
   }
 
   parseLines(lines: string[]) {
-    const dirLine = lines.shift()?.split("");
-    this.dirs = dirLine as Dir[];
-    lines.shift();
-
-    for (const line of lines) {
-      const [nodeId, childIds] = line.split(" = ");
-      const [L, R] = childIds.slice(1, -1).split(", ");
-
-      this.nodes[nodeId] = { L, R };
-
-      if (nodeId.endsWith("A")) {
-        this.nodeIds.push(nodeId);
-      }
-    }
+    this.seqs = lines.map((line) => line.split(" ").map(Number));
   }
 }
