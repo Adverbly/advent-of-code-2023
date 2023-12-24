f126944c153c311301801c122d66c0e01cdd5642
George Montgomery <george.p.montgomery@gmail.com>
2023-12-24 11:32:08 -0500

    d23p2 ~20s

--- solutions/day-23/part-2/Solver.ts
+++ solutions/day-23/part-2/Solver.ts
@@ -1,7 +1,6 @@
 type Cell = number;
 type RowCol = [number, number];
 type Adj = {
-  seen: Set<string>;
   value: Cell;
   rowCol: RowCol;
   parent: Adj | null;
@@ -14,6 +13,7 @@ type Node = {
 };
 type SearchNode = Node & {
   visited: NodeId[];
+  visitingDistance: number;
 };
 
 const UP = 5;
@@ -28,12 +28,14 @@ export class Solver {
   start: RowCol = [0, 1];
   root!: Adj;
   rootNode!: Node;
+  startNode!: Node;
+  endNode!: Node;
   nodes: Record<NodeId, Node> = {};
+  maxFound: number = 0;
 
   constructor(lines: string[]) {
     this.parseLines(lines);
     this.root = {
-      seen: new Set(),
       value: EMPTY,
       distance: 0,
       rowCol: [0, 1],
@@ -68,57 +70,16 @@ export class Solver {
       n.value !== FOREST && n.value !== undefined && result.push(n);
     }
 
-    let seen = adj.seen;
-
     result = result.filter(
       (res) =>
         !adj.parent ||
-        (this.cellKey(adj.parent.rowCol) !== this.cellKey(res.rowCol) &&
-          !seen.has(this.cellKey(res.rowCol)))
+        this.cellKey(adj.parent.rowCol) !== this.cellKey(res.rowCol)
     );
-    if (result.length === 1) {
-      const key = this.cellKey(result[0].rowCol);
-      if (seen.has(key)) {
-        return [];
-      }
-      seen.add(key);
-      return result.map((res) => ({
-        ...res,
-        distance: adj.distance + 1,
-        parent: adj,
-        seen,
-      }));
-    } else {
-      return result
-        .filter((res) => {
-          // TODO: Use entirely different approach where I parse the grid and create a graph to find the branching points, and then do a path cost maximization where each edge has weight equal to the length to the next node.
-
-          // TODO: Idea to improve perf here: After each split, do a BFS to see if the end is still reachable. When its not reachable, filter out that branch.
-          const foundEnd = this.dfsGridForEnd({
-            ...res,
-            seen: new Set([...seen, this.cellKey(res.rowCol)]),
-            distance: 0,
-            parent: adj,
-          });
-          // if (!foundEnd) {
-          //   console.log("didnt find end!", this.cellKey(res.rowCol));
-          // } else {
-          //   console.log("Can still find end...", this.cellKey(res.rowCol));
-          // }
-
-          return foundEnd;
-        })
-        .map((res) => {
-          // console.log("Splitting at", res.rowCol, result.length)c;
-          const newSeen = new Set([...seen, this.cellKey(res.rowCol)]);
-          return {
-            ...res,
-            distance: adj.distance + 1,
-            parent: adj,
-            seen: newSeen,
-          };
-        });
-    }
+    return result.map((r) => ({
+      ...r,
+      parent: adj,
+      distance: adj.distance + 1,
+    }));
   }
 
   adjCell(mid: RowCol, diff: RowCol) {
@@ -137,59 +98,12 @@ export class Solver {
     return JSON.stringify(rowCol);
   }
 
-  isAncestor({ seen }: Adj, candidateKey: string): boolean | null {
-    return seen.has(candidateKey);
-  }
-
-  bfsGrid({
-    horizon,
-    forEach,
-  }: {
-    horizon: Adj[];
-    forEach: (cell: Adj, children: Adj[]) => void;
-  }) {
-    while (horizon.length > 0) {
-      const currentAdj = horizon.shift()!;
-
-      const adjCells = this.adjCells(currentAdj);
-      forEach(currentAdj, adjCells);
-
-      adjCells.forEach((adj, index) => {
-        horizon.push(adj);
-      });
-    }
-  }
-
-  printGrid() {
-    console.log(this.grid.map((row) => row.join("")).join("\n"));
-  }
-
-  dfsGridForEnd(node: Adj): boolean {
-    if (
-      node.rowCol[0] === this.grid.length - 1 &&
-      node.rowCol[1] === this.grid[0].length - 2
-    ) {
-      return true;
-    }
-    const children = this.adjCells(node);
-    children.forEach((c) => node.seen.add(this.cellKey(c.rowCol)));
-    return children.some((child) =>
-      this.dfsGridForEnd({
-        ...child,
-        distance: node.distance + 1,
-        parent: node,
-      })
-    );
-  }
-
   followEdge(start: Adj) {
-    console.log(start);
-
-    let startNodeId = this.cellKey(start.rowCol);
+    let startNodeId = this.cellKey(start.parent!.rowCol);
     let startNode = this.nodes[startNodeId];
     let adj = start;
     let adjs = this.adjCells(start);
-    let edgeDistance = 0;
+    let edgeDistance = 1;
     while (adjs.length === 1) {
       edgeDistance++;
       adj = adjs[0];
@@ -213,67 +127,52 @@ export class Solver {
     nextAdjs.forEach((nextAdj) => this.followEdge(nextAdj));
   }
 
-  ancestors(node: Adj): Adj[] {
-    return [node, ...(node.parent ? this.ancestors(node.parent) : [])];
-  }
-
   solve() {
-    let longestResult = this.root;
-    const startNode: Node = {
+    this.startNode = {
       distToNeighbor: {},
       id: this.cellKey(this.root.rowCol),
     };
-    const endNode: Node = {
+    this.endNode = {
       distToNeighbor: {},
       id: this.cellKey([this.grid.length - 1, this.grid[0].length - 2]),
     };
-    this.nodes[startNode.id] = startNode;
-    this.nodes[endNode.id] = endNode;
+    this.nodes[this.startNode.id] = this.startNode;
+    this.nodes[this.endNode.id] = this.endNode;
     const startingEdge = this.adjCells(this.root)[0];
     this.followEdge(startingEdge);
-    console.log(this.nodes);
 
-    // this.bfsGrid({
-    //   horizon: [this.root],
-    //   forEach: (cell, children) => {
-    //     if (children.length === 0) {
-    //       console.log(cell.distance);
-    //     }
-    //     if (
-    //       children.length === 0 &&
-    //       cell.seen.has(
-    //         this.cellKey([this.grid.length - 1, this.grid[0].length - 2])
-    //       )
-    //     ) {
-    //       console.log("valid!", cell.distance);
-    //       longestResult =
-    //         longestResult.distance < cell.distance ? cell : longestResult;
-    //     }
-    //   },
-    // });
-    // this.printGridAncestors(longestResult);
-
-    return longestResult.distance;
+    const searchNode: SearchNode = {
+      ...this.startNode,
+      visited: [this.startNode.id],
+      visitingDistance: 0,
+    };
+    this.maxDistToEndFrom(searchNode);
+    return this.maxFound;
   }
-  printGridAncestors(longestResult: Adj) {
-    const ancestors = this.ancestors(longestResult).map((a) =>
-      this.cellKey(a.rowCol)
-    );
-    console.log(
-      this.grid
-        .map((row, rowIndex) =>
-          row
-            .map((c, colIndex) =>
-              ancestors.includes(this.cellKey([rowIndex, colIndex]))
-                ? "O"
-                : c === 1
-                ? "#"
-                : "."
-            )
-            .join("")
-        )
-        .join("\n")
-    );
+
+  maxDistToEndFrom(searchNode: SearchNode) {
+    Object.entries(searchNode.distToNeighbor).map(([neighborId, distance]) => {
+      if (searchNode.visited.includes(neighborId)) {
+        return;
+      }
+      if (neighborId === this.endNode.id) {
+        this.maxFound = Math.max(
+          this.maxFound,
+          distance + searchNode.visitingDistance
+        );
+        return;
+      }
+
+      const visited = searchNode.visited;
+      visited.push(neighborId);
+      const newSearch: SearchNode = {
+        ...this.nodes[neighborId],
+        visited,
+        visitingDistance: distance + searchNode.visitingDistance,
+      };
+      this.maxDistToEndFrom(newSearch);
+      visited.pop();
+    });
   }
 
   parseLines(lines: string[]) {
